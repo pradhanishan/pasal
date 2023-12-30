254c70bd1ae3e38e5227f4cf9cb89db4ce7d6f26
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2023-12-30 20:55:56 +0700

    add light and dark mode provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import ToggleThemeButton from "@/components/toggle-theme-button";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <ToggleThemeButton />
+      Hello world
+    </div>
+  );
 }
