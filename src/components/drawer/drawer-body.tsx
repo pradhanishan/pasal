f506fa7a8bd06f607ecc54f711748bcb5f9bfeb9
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2023-12-31 11:12:50 +0700

    before auth

--- src/components/drawer/drawer-body.tsx
+++ src/components/drawer/drawer-body.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import UserCard from '../user-card';
 import DrawerStack from './drawer-stack';
 import { drawerStacks } from '@/data/drawer-stacks';
 type TDrawerBodyProps = {
@@ -15,6 +16,7 @@ export default function DrawerBody({ handleCloseDrawer }: TDrawerBodyProps) {
       {/* drawer body */}
       <div className='flex w-full flex-col  justify-start space-y-8 overflow-y-auto bg-emerald-300 px-8 pt-32 shadow-lg sm:w-3/4 md:w-1/2 lg:w-1/4 dark:bg-zinc-900'>
         <div className='flex flex-col space-y-16'>
+          <UserCard />
           {/* Drawer stacks and items */}
           {Array.from(drawerStacks).map(([key, value]) => {
             return (
---
+++ src/components/user-card.tsx
@@ -0,0 +1,13 @@
+export default function UserCard() {
+  return (
+    <div className='flex min-h-80 flex-col items-center justify-start rounded-md border'>
+      <div></div>
+      <h1>username</h1>
+      <ul>
+        <li>cart</li>
+        <li>orders</li>
+        <li>account</li>
+      </ul>
+    </div>
+  );
+}
